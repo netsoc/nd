fee1f60afc1ae0be0ed8b8191b9d83edc44bac7b
Colm Vize <vizec@tcd.ie>
2014-10-13 15:09:27 +0100

    modified sendmail.py so that it just sends the mail and accepts a message as a parameter

--- sendmail.py
+++ sendmail.py
@@ -1,36 +1,15 @@
 # Python interface to /usr/lib/sendmail
 
-import email
-import email.parser
 import subprocess
-import os.path
 
 default_from_address = "support@netsoc.tcd.ie"
-default_template_location =\
-    os.path.abspath(os.path.dirname(__file__)) + "/messages"
 
 
-def sendmail(template_file, dict=None, **kwargs):
+def sendmail(msg, dict=None, **kwargs):
 
     if dict is None:
         dict = {}
     dict.update(kwargs)
-    if template_file[0] != "/":
-        try:
-            txtmsg = open(default_template_location +
-                          "/" + template_file, "r").read()
-        except Exception, e:
-            txtmsg = open(template_file, "r").read()
-    else:
-        txtmsg = open(template_file, "r").read()
-    try:
-        txtmsg = txtmsg % dict
-    except KeyError, e:
-        raise Exception(
-            "Required field %s must be included for this template" % e)
-
-    msg = email.parser.Parser().parsestr(txtmsg)
-
     if 'From' not in msg:
         if 'From' in dict:
             msg['From'] = dict['From']
@@ -38,7 +17,7 @@ def sendmail(template_file, dict=None, **kwargs):
             msg['From'] = default_from_address
     if 'To' not in msg and 'To' in dict:
         msg['To'] = dict['To']
-    if 'Subject not in msg' and 'Subject' in dict:
+    if 'Subject' not in msg and 'Subject' in dict:
         msg['Subject'] = dict['Subject']
 
     for h in ['From', 'To', 'Subject']:
@@ -54,5 +33,8 @@ def sendmail(template_file, dict=None, **kwargs):
         print "Sending mail from %s to %s" %\
             (msg['From'], msg['To'])
         sendmail = subprocess.Popen(
-            ["/usr/lib/sendmail", "-f", msg['From']], stdin=subprocess.PIPE)
+            ["/usr/lib/sendmail",
+             kwargs["To"],
+             msg['From']],
+            stdin=subprocess.PIPE)
         sendmail.communicate(msg.as_string())
